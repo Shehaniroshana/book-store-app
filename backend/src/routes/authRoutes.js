92504f90819ab9d3c86754b1d182e742667187d3
Shehaniroshana <shehanshehaniroshan@gmail.com>
2025-04-22 19:37:39 +0530

    Implement user registration and authentication with JWT; add User model and password hashing

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,70 @@
 import express from "express";
+import User from "../models/User.js";
 const router = express.Router();
+import jwt from "jsonwebtoken"
+
+const generateToken=(id)=>{
+   return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:"15d"});
+}
+
 
 router.get("/login", async (req, res) => {
+    
     res.send("login")
 })
 
-router.get("/register", async (req, res) => {
-    res.send("register")
+router.post("/register", async (req, res) => {
+    try{
+
+        const {email,username,password}=req.body
+        if(!email || !username || !password){
+            return res.status(400).send("All fields are required")
+        }
+
+        if(password.length<6){
+            return res.status(400).send("Password must be at least 6 characters")   
+        }
+
+        if(username.length<3){
+            return res.status(400).send("Username must be at least 3 characters")
+        }
+
+        //check if user already exists
+            const existingEmail=await User.findOne({email});
+            if(existingEmail){
+                return res.status(400).send("User already exists")    
+            }
+
+            const existingUsername=await User.findOne({username});
+            if(existingUsername){
+                return res.status(400).send("User already exists")    
+            }
+           
+            //generate profile image
+           const profileImage=`https://api.dicebear.com/6.x/initials/svg?seed=${username}`;
+
+           const user= new User({
+             email,
+             username,
+             password,
+             profileImage,
+           })
+
+           
+           await user.save();
+            
+           const token=generateToken(user._id);
+           res.status(201).send({user:{
+            _id:user._id,
+            username:user.username,
+            email:user.email,
+            profileImage:user.profileImage
+           },token});
+      
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"Internal Server Error"});
+    }
 })
 
 export default router
